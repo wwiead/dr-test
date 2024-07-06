5bbded40790013971e1dbc5701b37298fd9942c3
Alireza Shirzad <wwwiead69@gmail.com>
2024-07-06 22:39:54 +0300

    d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1545936149"
+    serviceWorkerVersion: "3537977139"
   }
 });
